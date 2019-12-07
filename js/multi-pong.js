3e2c3d090acf06284bbd8355f1364f9cec3eff1b
Brook Rodia <brookrodia@gmail.com>
2019-12-07 00:58:15 -0500

    Added Client server basics

--- js/multi-pong.js
+++ js/multi-pong.js
@@ -1,3 +1,15 @@
+// Connect to Server with Colyseus.js
+import * as Colyseus from "colyseus.js";
+
+var client = new Colyseus.Client('ws://localhost:5050');
+
+//Join a room
+client.join("test_room").then(room => {
+    console.log(room.sessionId, "joined", room.name);
+}).catch(e => {
+    console.log("JOIN ERROR", e);
+});
+
 //Create reusable vector
 class Vec {
     constructor(x = 0, y = 0) {
