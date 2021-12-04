8529964ffacea8b8cf3f10e6a5736fda6cee0139
Mike Varga <mvarga6@kent.edu>
2021-12-04 12:04:37 -0500

    Add basic auth to admin colyseus page

--- server.ts
+++ server.ts
@@ -5,42 +5,48 @@ import express from "express";
 import cors from "cors";
 import { Server } from "colyseus";
 import { monitor } from "@colyseus/monitor";
-// import socialRoutes from "@colyseus/social/express"
+import  basicAuth  from "express-basic-auth";
 
 import { ChatRoom } from "./ts/GameRooms/ChatRoom";
 import { PongRoom } from './ts/GameRooms/Pong/PongRoom';
 
-const port = Number(process.env.PORT || 2567);
-const app = express()
+// Load environment
+const PORT = Number(process.env.PORT || 2567)
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'password'
 
+// Create express app + middleware
+const app = express()
 app.use(cors());
 app.use(express.json())
 
+// Configure Colyseus game server
 const gameServer = new Server({
   server: http.createServer(app),
   express: app,
 });
 
-// register your room handlers
+// Register your Colyseus room handlers
 gameServer.define('chat', ChatRoom);
 gameServer.define('pong', PongRoom);
-
-// TODO: This only works if we have DB setup
-// register @colyseus/social routes
-// app.use("/", socialRoutes);
 
 // For serving the html docs for the rooms
 app.use('/', express.static(path.join(__dirname, "static")));
 app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
 app.use('/js', express.static(path.join(__dirname, "js")));
 
-// register colyseus monitor AFTER registering your room handlers
-app.use("/colyseus", monitor(gameServer));
+
+// Basic auth protected colyseus monitor (must be AFTER registering your room handlers)
+app.use(
+  "/colyseus",
+  basicAuth({ users: { 'admin': ADMIN_PASSWORD }, challenge: true, realm: "multi-pong-server" }),
+  monitor(gameServer)
+);
 
 gameServer.onShutdown(function(){
   console.log(`game server is going down.`);
 });
 
-gameServer.listen(port);
+// Run the game server async in background
+gameServer.listen(PORT);
 
-console.log(`Listening on http|ws://localhost:${ port }` )
+console.log(`Listening on http|ws://localhost:${ PORT }` )
