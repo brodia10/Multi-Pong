4a9cef2e23527e7f324d400b38bdd31ecb7286b0
Mike Varga <mvarga6@kent.edu>
2021-12-04 17:18:02 -0500

    Add template engine (#28)
    * Add eta template package
    
    * Move client JS files under static/js
    
    * Convert HTML files to ETA templates
    
    * Refactor our application routes out of server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import path from 'path';
 import http from "http";
-import serveIndex from 'serve-index';
 import express from "express";
 import cors from "cors";
 import { Server } from "colyseus";
 import { monitor } from "@colyseus/monitor";
 import  basicAuth  from "express-basic-auth";
+import * as Eta from "eta"
 
+import ApplicationRoutes from "./routes";
 import { ChatRoom } from "./ts/GameRooms/ChatRoom";
 import { PongRoom } from './ts/GameRooms/Pong/PongRoom';
 
@@ -15,11 +15,22 @@ const PORT = Number(process.env.PORT || 2567)
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'password'
 
 // Create express app + middleware
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-// Configure Colyseus game server
+// Configure template engine
+app.engine("eta", Eta.renderFile);
+app.set("view engine", "eta")
+app.set("views", "./views")
+
+// Add our application to the server
+app.use('/', ApplicationRoutes);
+
+/*
+* Configure Colyseus game server
+*/
+
 const gameServer = new Server({
   server: http.createServer(app),
   express: app,
@@ -28,12 +39,6 @@ const gameServer = new Server({
 // Register your Colyseus room handlers
 gameServer.define('chat', ChatRoom);
 gameServer.define('pong', PongRoom);
-
-// For serving the html docs for the rooms
-app.use('/', express.static(path.join(__dirname, "static")));
-app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
-app.use('/js', express.static(path.join(__dirname, "js")));
-
 
 // Basic auth protected colyseus monitor (must be AFTER registering your room handlers)
 app.use(
@@ -49,4 +54,4 @@ gameServer.onShutdown(function(){
 // Run the game server async in background
 gameServer.listen(PORT);
 
-console.log(`Listening on http|ws://localhost:${ PORT }` )
+console.log(`Listening on http|ws://localhost:${ PORT }` );
